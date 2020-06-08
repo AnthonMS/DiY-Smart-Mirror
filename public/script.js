3819b79d7b7cc96f2ce1e6e4483292c77c2bb641
AnthonMS <Anthon@Steiness.info>
2020-06-08 18:26:53 +0200

    test commit 2

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 function init() {
     // Import Container Manager Script
     var import_container_manager = document.createElement('script');
-    import_container_manager.src = '/container_manager_script.js';
+    import_container_manager.src = '/scripts/container_manager_script.js';
     document.head.appendChild(import_container_manager);
 
     // Import Menu Manager Script
     var import_menu_manager = document.createElement('script');
-    import_menu_manager.src = '/menu_manager_script.js';
+    import_menu_manager.src = '/scripts/menu_manager_script.js';
     document.head.appendChild(import_menu_manager);
 }
