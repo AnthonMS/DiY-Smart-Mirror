b26bc60a1e83da72b45671c994d8af4134ea05de
AnthonMS <Anthon@Steiness.info>
2020-06-20 17:30:50 +0200

    Added color selection for light with RGB support. If the light does not have rgb support, the butotn will not show up underneath the slider. But every lightbox needs to have this color input none the less. This way the controller will handle if the dimmer needs color input as well, it will check if the entity supports it through the API.

--- public/plugins/home-assistant/controller.js
+++ public/plugins/home-assistant/controller.js
@@ -11,11 +11,41 @@ haContainer.style.top = '45px';
 var apiToken = '';
 var haUrl = 'http://192.168.0.10:8123/api';
 
-
 getDimmerStates();
 
+//colorPicker.addEventListener("change", watchColorPicker, false);
+var colorPickers = document.getElementsByClassName('color_picker');
+for (var i = 0; i < colorPickers.length; i++) {
+    colorPickers[i].addEventListener('input', (event) => {
+        var rgbValue = hexToRgb(event.target.value);
+        var lightBox = event.target.parentElement.parentElement;
+        var entityId = lightBox.getElementsByClassName('light_dimmer')[0].id;
+
+        var payload = {
+            'entity_id': entityId,
+            'rgb_color': [rgbValue.r, rgbValue.g, rgbValue.b]
+        };
+        postData(haUrl, '/services/light/turn_on', payload)
+            .then(data => {});
+
+    });
+    // colorPickers[i].addEventListener('change', (event) => {
+    //     console.log('change');
+    //     console.log(event);
+    // });
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+// Called when sliders change value
 function onInputChange(value, sliderId) {
-    // console.log(sliderId + ': ' + value);
     var dimmer = document.getElementById(sliderId);
     var lightPercentage = dimmer.parentElement.parentElement.getElementsByClassName('light_percentage')[0];
     lightPercentage.innerHTML = value + '%';
@@ -25,11 +55,22 @@ function onInputChange(value, sliderId) {
         'entity_id': sliderId,
         'brightness': value * 2.54
     };
-    postData(haUrl, '/services/light/turn_on', payload).then(data => {});
-    
+    postData(haUrl, '/services/light/turn_on', payload)
+        .then(data => {
+            // Check if light entity has rgb support
+            // Show/Hide color picker if it does/'nt
+            if (data[0].attributes.rgb_color) {
+                toggleColorOption(dimmer.parentElement.parentElement, true);
+            }
+            else {
+                toggleColorOption(dimmer.parentElement.parentElement, false);
+            }
+        });
 }
 
-function getDimmerState() {
+// Called once when controller is loaded
+// Will get light states of dimmers
+function getDimmerStates() {
     var dimmers = document.getElementsByClassName('light_dimmer');
     for (var i = 0; i < dimmers.length; i++) {
         
@@ -41,27 +82,52 @@ function getDimmerState() {
 
 }
 
+// Called when getting dimmer states on controller load
 function setDimmerState(data) {
     var dimmer = document.getElementById(data.entity_id);
     var lightTitle = dimmer.parentElement.parentElement.getElementsByClassName('light_title')[0];
     var lightPercentage = dimmer.parentElement.parentElement.getElementsByClassName('light_percentage')[0];
     lightTitle.innerHTML = data.attributes.friendly_name + ': ';
-    console.log(data.entity_id);
     if (data.attributes.brightness === undefined) {
-        console.log('Light is off');
         dimmer.value = '0';
         lightPercentage.innerHTML = '0%';
+        // Hide color button and color picker, because light is off
+        toggleColorOption(dimmer.parentElement.parentElement, false);
     }
     else {
         var brightness = Math.round(data.attributes.brightness / 2.54);
-        console.log(brightness);
         dimmer.value = brightness;
         lightPercentage.innerHTML = brightness + '%';
+
+        // Check if light entity has rgb support
+        if (data.attributes.rgb_color) {
+            // Show color button and color picker
+            toggleColorOption(dimmer.parentElement.parentElement, true);
+        }
+        else {
+            // Hide color button and color picker
+            toggleColorOption(dimmer.parentElement.parentElement, false);
+        }
     }
     
 }
 
+// This will Show/Hide color picker inside the div that is given in variable lightBox.
+function toggleColorOption(lightBox, show) {
+    if (show) {
+        var colorpicker = lightBox.getElementsByClassName('color_picker_div')[0];
+        //console.log(colorBtn);
+        colorpicker.style.display = 'block';
+    } 
+    else {
+        var colorpicker = lightBox.getElementsByClassName('color_picker_div')[0];
+        //console.log(colorBtn);
+        colorpicker.style.display = 'none';
 
+    }
+}
+
+// This will GET the state of an entity specified
 async function getState(url, uri, entity) {
     // Default options are marked with *
     const response = await fetch(url + uri + entity, {
@@ -78,7 +144,7 @@ async function getState(url, uri, entity) {
 }
 
 
-// Example POST method implementation:
+// This will POST data to given service (uri) with the payload given (data)
 async function postData(url = '', uri, data = {}) {
     // Default options are marked with *
     const response = await fetch(url + uri, {
@@ -90,5 +156,6 @@ async function postData(url = '', uri, data = {}) {
         },
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+    const myJson = await response.json();
+    return myJson; // parses JSON response into native JavaScript objects
 }
