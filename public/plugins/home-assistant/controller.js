5c7c4a0352407be448ee044ca68b98fac06bde40
AnthonMS <Anthon@Steiness.info>
2020-06-20 15:53:26 +0200

    HA GetStates functions. This will go through dimmers id and get the states for the entities. It will then set the dimmer title as the entities friendly name, the percentage of the lights brightness state. If the light is off, it will of course be set to 0%

--- public/plugins/home-assistant/controller.js
+++ public/plugins/home-assistant/controller.js
@@ -2,14 +2,17 @@ console.log('PLUGIN: Home Assistant');
 
 var haContainer = document.getElementById('ha_container');
 
-haContainer.style.display = "none";
+haContainer.style.display = "block";
 haContainer.style.width = "200px";
 haContainer.style.height = "350px";
-haContainer.style.left = '720px';
-haContainer.style.top = '120px';
+haContainer.style.left = '610px';
+haContainer.style.top = '45px';
 
 var apiToken = '';
-var haUrl = 'http://192.168.0.10:8123/api/services/light/turn_on';
+var haUrl = 'http://192.168.0.10:8123/api';
+
+
+getDimmerStates();
 
 function onInputChange(value, sliderId) {
     // console.log(sliderId + ': ' + value);
@@ -20,19 +23,65 @@ function onInputChange(value, sliderId) {
 
     var payload = {
         'entity_id': sliderId,
-        'brightness': value * 2.55
+        'brightness': value * 2.54
     };
-    postData(haUrl, payload).then(data => {});
+    postData(haUrl, '/services/light/turn_on', payload).then(data => {});
+    
+}
+
+function getDimmerState() {
+    var dimmers = document.getElementsByClassName('light_dimmer');
+    for (var i = 0; i < dimmers.length; i++) {
+        
+        getState(haUrl, '/states/', dimmers[i].id)
+            .then(json => {
+                setDimmerState(json);
+            });
+    }
+
+}
+
+function setDimmerState(data) {
+    var dimmer = document.getElementById(data.entity_id);
+    var lightTitle = dimmer.parentElement.parentElement.getElementsByClassName('light_title')[0];
+    var lightPercentage = dimmer.parentElement.parentElement.getElementsByClassName('light_percentage')[0];
+    lightTitle.innerHTML = data.attributes.friendly_name + ': ';
+    console.log(data.entity_id);
+    if (data.attributes.brightness === undefined) {
+        console.log('Light is off');
+        dimmer.value = '0';
+        lightPercentage.innerHTML = '0%';
+    }
+    else {
+        var brightness = Math.round(data.attributes.brightness / 2.54);
+        console.log(brightness);
+        dimmer.value = brightness;
+        lightPercentage.innerHTML = brightness + '%';
+    }
     
 }
 
 
+async function getState(url, uri, entity) {
+    // Default options are marked with *
+    const response = await fetch(url + uri + entity, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            "Authorization": "Bearer " + apiToken,
+            'Content-Type': 'application/json',
+            'origin': 'http://localhost:8080/'
+        }
+    });
+    const myJson = await response.json();
+    return myJson; // parses JSON response into native JavaScript objects
+
+}
 
 
 // Example POST method implementation:
-async function postData(url = '', data = {}) {
+async function postData(url = '', uri, data = {}) {
     // Default options are marked with *
-    const response = await fetch(url, {
+    const response = await fetch(url + uri, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         headers: {
             "Authorization": "Bearer " + apiToken,
